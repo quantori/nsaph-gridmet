696bb79cb22774ad0e28a388f80b5da1a5681871
mmcentre <michael.bouzinier@kvitneset.com>
2021-06-17 15:42:27 -0400

    MAB   Updating documentation to describe output format

--- src/python/gridmet.py
+++ src/python/gridmet.py
@@ -9,6 +9,23 @@
     Which geography is used is defined by `geography` argument that defaults
     to "zip". Only actually used geographies must have their shape files
     for the years actually used.
+
+    Output file format:
+    At the moment output is a simple 3+ columns file (most files contain 3
+    columns, but parameter “metadata” can define more columns to include):
+
+    1. Variable (aka band) mean value. The actual band is given in the arguments
+       (or configuration object) and is printed in the header line of the file
+
+    2. Date in YYYY-mm-dd format (SQL date format)
+
+    3. Label, associated with location. E.g., zip code for zip shapes,
+       county fips for county shapes or custom label for point file.
+       For points file, the label is taken from the first column defined by
+       “metadata” argument.
+
+    4+. If more than one column is included in metadata, the output file
+        will contain more than 3 columns
 """
 import os
 from typing import List
